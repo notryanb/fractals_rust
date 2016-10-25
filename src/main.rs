fe9706f028097e15d845d1eeca50581ace2a39b9
Ryan B <notryanb@gmail.com>
2016-10-25 11:19:33 -0400

    Add image buffer with one black pixel and points array

--- src/main.rs
+++ src/main.rs
@@ -18,5 +18,21 @@ const HEIGHT: u32 = 600;
 
 /// main program
 pub fn main() {
+    let mut img = image::ImageBuffer::from_fn(WIDTH, HEIGHT, |x, y| {
+        if x == 0 && y == 0 {
+            image::Luma([0u8])
+        } else {
+            image::Luma([255u8])
+        }
+    });
+
+    let mut counter: u32 = 10_000;
+
+    let pts: [Point; 3] = [
+            Point { x: WIDTH / 2, y: 0 },
+            Point { x: 0, y: HEIGHT },
+            Point { x: WIDTH, y: HEIGHT }
+    ];
+
 
 }

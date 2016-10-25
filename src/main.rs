6030f1e7e58fb2b8b79f71324975ea06aabeef39
Ryan B <notryanb@gmail.com>
2016-10-25 11:13:57 -0400

    Add Point struct

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,19 @@
-extern image;
-extern rand;
+//! sierpinsky triangle image generator
 
+extern crate image;
+extern crate rand;
+
+use rand::Rng;
+use std::fs::File;
+use std::path::Path;
+
+/// points used for building triangle and plot on canvas
+pub struct Point{
+    x: u32,
+    y: u32
+}
+
+/// main program
 pub fn main() {
 
 }
